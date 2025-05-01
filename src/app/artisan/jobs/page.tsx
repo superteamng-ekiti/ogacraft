c0405260e60d8cb324e270e88c2ae8cdbdd203ef
EmmanuelOluwafemi <emaz4me@gmail.com>
2025-05-01 05:35:53 +0100

    update

--- src/app/artisan/jobs/page.tsx
+++ src/app/artisan/jobs/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { JobBreadCrumb } from './_components/job-breadcrumb'
+import { JobsWrapper } from './_components/jobs-wrapper'
 
 const Jobs = () => {
   return (
-    <div className='container px-4 md:px-0 mx-auto py-8'>
+    <div className='container px-4 md:px-0 mx-auto py-8 flex flex-col gap-4'>
         <JobBreadCrumb />
+        <JobsWrapper />
     </div>
   )
 }
